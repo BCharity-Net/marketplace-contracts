3b41a4385730acd9e386cd23fd22c2224e4de927
NonBenevolentPotato <85574012+PerfectlyHarmlessPotato@users.noreply.github.com>
2021-07-23 13:11:15 -0400

    Created skeleton functions
    Set up various skeleton functions for future implementation.

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,109 @@
+// solhint-disable not-rely-on-time
+pragma solidity ^0.6.6;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+import "./PurchaseListener.sol";
+import "./Ownable.sol";
+
+interface IGIVEMarketplace{
+
+}
+
+/*
+ *
+ */
+
+contract GIVEMarketplace is Ownable, IGIVEMarketplace{
+	using SafeMath for int256
+
+	//events
+
+	
+	//Structures
+	struct Asset{
+	
+	}
+
+	struct Order{
+	
+	}
+
+	// Marketplace Lifecycle
+
+	ERC20 public paymentToken;
+
+	constructor() Ownable() public{
+		
+	}
+
+	function _initialize() internal {
+	
+	}
+
+	/// Asset management
+
+	function getAsset(){
+	
+	}
+
+	function getAssetLocal(){
+	
+	}
+
+	modifier onlyProductOwner(){
+	
+	}
+
+	function _importAssetIfNeeded(){
+	
+	}
+
+	function createAsset(){
+	
+	}
+
+	function updateAsset(){
+	
+	}
+
+	//two step asset transfer method
+	function offerAsset(){
+	
+	}
+
+	function claimAsset(){
+	
+	}
+
+	//one step asset transfer method
+	function transferAsset(){
+	
+	}
+
+	//Whitelist mangement, if necessary?  
+
+	// Order management
+
+	function getOrder(){
+		
+	}
+
+	function getOrderLocal(){
+	
+	}
+
+	function createOrder(){
+		
+	}
+
+	function cancelOrder (){
+	
+	}
+
+	function fulfillOrder(){
+	
+	}
+	
+}
