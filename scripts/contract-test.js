07081d469032620e9647d4c81a6f9c8515109267
stalkeraturdoor <tonyshi12@gmail.com>
2021-07-29 16:48:00 -0600

    scrap gas estimate, switch mnemonic => private key

--- scripts/contract-test.js
+++ scripts/contract-test.js
@@ -1,6 +1,6 @@
 const API_URL = "https://polygon-mumbai.g.alchemy.com/v2/Nfq9kasfoYe8wbUKeAgXDPTi1VAROBZA";
 const PUBLIC_KEY = process.env.PUBLIC_KEY;
-const MNEMONIC = process.env.MNEMONIC;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(API_URL);
@@ -12,19 +12,19 @@ const NFTMarketplaceContract = new web3.eth.Contract(contract.abi, contractAddre
 
 async function createNFT(assetId, tokenId, nftContract, numSales, royalties, assetOwner, creator) {
     const nonce = await web3.eth.getTransactionCount(PUBLIC_KEY, 'latest'); // get latest nonce
-    const gasEstimate = await NFTMarketplaceContract.methods.createAsset(assetId, tokenId, nftContract, numSales, royalties, assetOwner, creator).estimateGas(); // estimate gas
+    // const gasEstimate = await NFTMarketplaceContract.methods.createAsset(assetId, tokenId, nftContract, numSales, royalties, assetOwner, creator).estimateGas(); // estimate gas
 
     // Create the transaction
     const tx = {
       'from': PUBLIC_KEY,
       'to': contractAddress,
       'nonce': nonce,
-      'gas': gasEstimate, 
+      'gas': 2000000, 
       'data': NFTMarketplaceContract.methods.createAsset(assetId, tokenId, nftContract, numSales, royalties, assetOwner, creator).encodeABI()
     };
 
     // Sign the transaction
-    const signPromise = web3.eth.accounts.signTransaction(tx, MNEMONIC);
+    const signPromise = web3.eth.accounts.signTransaction(tx, PRIVATE_KEY);
     signPromise.then((signedTx) => {
       web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(err, hash) {
         if (!err) {
@@ -39,8 +39,10 @@ async function createNFT(assetId, tokenId, nftContract, numSales, royalties, ass
 }
 
 async function main() {
-
-    await createNFT(1, 1, "0x686A02772F06c0E1208aA6549e1f40af749C2CE8", 0, 900, "0x11f408335E4B70459dF69390ab8948fcD51004D0", "0x11f408335E4B70459dF69390ab8948fcD51004D0");
+  one = web3.utils.toHex(1);
+  passone = web3.utils.padRight(one, 32);
+  console.log(passone);
+  await createNFT(passone, 1, "0x686A02772F06c0E1208aA6549e1f40af749C2CE8", 0, 900, "0x11f408335E4B70459dF69390ab8948fcD51004D0", "0x11f408335E4B70459dF69390ab8948fcD51004D0");
 }
 
 main();
